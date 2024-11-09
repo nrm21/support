b801ab71d51447eb423f9cd2226f8787f4b51037
nrm21 <natemorrison@gmail.com>
2024-11-09 18:01:39 -0500

    Make package names exportable

--- selenium.go
+++ selenium.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Loops until we find an element and return it (this might need a timeout later on)
-func loopUntilFindRowElement(we selenium.WebElement, cssselector string) selenium.WebElement {
+func LoopUntilFindRowElement(we selenium.WebElement, cssselector string) selenium.WebElement {
 	elem, err := we.FindElement(selenium.ByCSSSelector, cssselector)
 	for err != nil {
 		time.Sleep(time.Millisecond * 500)
@@ -19,7 +19,7 @@ func loopUntilFindRowElement(we selenium.WebElement, cssselector string) seleniu
 }
 
 // Loops until we find an element and return it (this might need a timeout later on)
-func loopUntilFindElement(driver *selenium.WebDriver, cssselector string) selenium.WebElement {
+func LoopUntilFindElement(driver *selenium.WebDriver, cssselector string) selenium.WebElement {
 	elem, err := (*driver).FindElement(selenium.ByCSSSelector, cssselector)
 	for err != nil {
 		time.Sleep(time.Millisecond * 500)
@@ -31,7 +31,7 @@ func loopUntilFindElement(driver *selenium.WebDriver, cssselector string) seleni
 }
 
 // Loops until we find a group of elements and return an array of them (this might need a timeout later on)
-func loopUntilFindElements(driver *selenium.WebDriver, cssselector string) []selenium.WebElement {
+func LoopUntilFindElements(driver *selenium.WebDriver, cssselector string) []selenium.WebElement {
 	elem, err := (*driver).FindElements(selenium.ByCSSSelector, cssselector)
 	for err != nil {
 		time.Sleep(time.Millisecond * 500)
